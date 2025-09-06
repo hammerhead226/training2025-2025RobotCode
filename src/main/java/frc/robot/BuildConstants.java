acc98e8ef61d9d646860b5354eca8394de207550
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-09-06 11:30:29 -0400

    They should clone this activity for the elastic activites

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "2025RobotCode";
+  public static final String MAVEN_NAME = "training2025-2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 565;
-  public static final String GIT_SHA = "8463307278e411664f457573b18346405fe3af3a";
-  public static final String GIT_DATE = "2025-08-16 13:47:50 EDT";
-  public static final String GIT_BRANCH = "MARC-2025";
-  public static final String BUILD_DATE = "2025-08-16 15:49:50 EDT";
-  public static final long BUILD_UNIX_TIME = 1755373790888L;
+  public static final int GIT_REVISION = 567;
+  public static final String GIT_SHA = "df68220f0ea3278e093f414560782e5f7f2c9125";
+  public static final String GIT_DATE = "2025-08-26 16:22:01 EDT";
+  public static final String GIT_BRANCH = "driverAutoWeekDay7";
+  public static final String BUILD_DATE = "2025-09-06 11:26:47 EDT";
+  public static final long BUILD_UNIX_TIME = 1757172407423L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
