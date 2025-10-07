1990f9121cd7ee38f4685cec960c7778886029db
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-10-07 18:26:58 -0400

    quick update
    quick update

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -175,9 +175,9 @@ public class Robot extends LoggedRobot {
   @Override
   public void robotInit() {
     ReefPositionsUtil.printOffsetPoses();
-    UsbCamera cam = CameraServer.startAutomaticCapture();
+    //UsbCamera cam = CameraServer.startAutomaticCapture();
 
-    cam.setResolution(640, 480);
+    //cam.setResolution(640, 480);
   }
 
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
