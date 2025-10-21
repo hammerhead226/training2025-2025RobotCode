29e321902698195ef7dbed17134940002ebd9e9a
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-10-21 18:40:25 -0400

    sim comp
    sim comp

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "training2025-2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 567;
-  public static final String GIT_SHA = "df68220f0ea3278e093f414560782e5f7f2c9125";
-  public static final String GIT_DATE = "2025-08-26 16:22:01 EDT";
+  public static final int GIT_REVISION = 568;
+  public static final String GIT_SHA = "acc98e8ef61d9d646860b5354eca8394de207550";
+  public static final String GIT_DATE = "2025-09-06 11:30:29 EDT";
   public static final String GIT_BRANCH = "driverAutoWeekDay7";
-  public static final String BUILD_DATE = "2025-09-06 11:26:47 EDT";
-  public static final long BUILD_UNIX_TIME = 1757172407423L;
+  public static final String BUILD_DATE = "2025-10-21 18:36:47 EDT";
+  public static final long BUILD_UNIX_TIME = 1761086207572L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
